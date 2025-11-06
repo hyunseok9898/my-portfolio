2ffa8eafbe8a2c6e2d84a4e62e44101c045d6707
hyunseok9898 <139948998+hyunseok9898@users.noreply.github.com>
2025-11-06 11:46:42 +0900

    404오류 해결

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 // src/index.tsx
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter  } from 'react-router-dom';
 import { RecoilRoot } from "recoil";
 import App from './App';
 
@@ -11,9 +11,9 @@ if (container) {
   root.render(
     <React.StrictMode>
        <RecoilRoot>
-          <BrowserRouter>
+          <HashRouter>
             <App />
-          </BrowserRouter>
+          </HashRouter>
        </RecoilRoot>
     </React.StrictMode>
   );
